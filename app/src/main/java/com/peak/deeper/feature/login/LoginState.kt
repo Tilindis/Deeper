25192433c5eb8ea69ded28110236393be4fde21f
D. Tilindis <heronightmares@gmail.com>
2024-09-01 23:30:34 +0300

    Add data store to login vm, update state, update login repo

--- app/src/main/java/com/peak/deeper/feature/login/LoginState.kt
+++ app/src/main/java/com/peak/deeper/feature/login/LoginState.kt
@@ -3,4 +3,5 @@ package com.peak.deeper.feature.login
 data class LoginState(
     val email: String = "deeperangler@gmail.com", // <-- Add test data,
     val password: String = "Deeper10899",         // <-- For faster checks.
+    val isUserLogged: Boolean? = null
 )
--- app/src/main/java/com/peak/deeper/feature/login/LoginViewModel.kt
+++ app/src/main/java/com/peak/deeper/feature/login/LoginViewModel.kt
@@ -2,6 +2,7 @@ package com.peak.deeper.feature.login
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.peak.deeper.utils.datastore.DataStore
 import com.peak.deeper.utils.interactor.LoginInteractor
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
@@ -14,9 +15,18 @@ import javax.inject.Inject
 @HiltViewModel
 class LoginViewModel @Inject constructor(
     private val loginInteractor: LoginInteractor,
+    private val dataStore: DataStore
 ) : ViewModel() {
     private val _state = MutableStateFlow(LoginState())
     val state = _state.asStateFlow()
+
+    init {
+        viewModelScope.launch {
+            dataStore.getLoginPassedValue().collect {
+                _state.update { state -> state.copy(isUserLogged = it) }
+            }
+        }
+    }
 
     fun updateEmail(value: String) {
         _state.update { it.copy(email = value) }
