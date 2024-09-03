2f5d32c3028580d9340870434755188471c78fd0
D. Tilindis <heronightmares@gmail.com>
2024-09-03 22:41:04 +0300

    Add new method to api of polygons

--- app/src/main/java/com/peak/deeper/utils/api/DeeperApi.kt
+++ app/src/main/java/com/peak/deeper/utils/api/DeeperApi.kt
@@ -1,14 +1,25 @@
 package com.peak.deeper.utils.api
 
 import com.peak.deeper.utils.request.LoginRequest
-import com.peak.deeper.utils.response.LoginResponse
+import com.peak.deeper.utils.response.bathymetry.GeoDataResponse
+import com.peak.deeper.utils.response.login.LoginResponse
 import retrofit2.Response
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Query
 
 interface DeeperApi {
     @POST("login")
     suspend fun postLoginRequest(
         @Body loginRequest: LoginRequest
     ): Response<LoginResponse>
+
+    @GET("geoData")
+    suspend fun getGeoData(
+        @Query("grid") grid: String = "FAST",
+        @Query("generator") generator: String = "BS",
+        @Query("scanIds") scanIds: String,
+        @Query("token") token: String
+    ): Response<GeoDataResponse>
 }
