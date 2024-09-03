2c3e69de46bc9ad83718d3d1b19cb5769995c717
D. Tilindis <heronightmares@gmail.com>
2024-09-03 22:38:42 +0300

    Add new colors and colors converter

---
+++ app/src/main/java/com/peak/deeper/utils/converter/color/ColorConverters.kt
@@ -0,0 +1,19 @@
+package com.peak.deeper.utils.converter.color
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.lerp
+import com.peak.deeper.ui.theme.Water
+import com.peak.deeper.ui.theme.WaterDeep
+
+class ColorConverters {
+    fun colorFromDepth(value: Double): Color {
+        return when (value) {
+            in 10.0..value -> WaterDeep
+            in value..0.0 -> Water
+            else -> {
+                val normalizedValue = value / 10
+                lerp(Water, WaterDeep, normalizedValue.toFloat())
+            }
+        }
+    }
+}
